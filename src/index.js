6f737cee5229e5afe53cf60640b96bdc0ff8db94
FabioRosado <fabioglrosado@gmail.com>
2019-01-15 22:34:09 +0000

    Set target price and get notification if met

--- src/index.js
+++ src/index.js
@@ -6,17 +6,9 @@ const store = new Store()
 const BrowserWindow = electron.remote.BrowserWindow
 const ipc = electron.ipcRenderer
 
-const notifyBtn = document.getElementById('notifyBtn')
-const targetPrice = document.getElementById('targetPrice')
-let targetPriceVal
 const currency = store.get('currency', 'GBP');
 const cryptocurrencies = store.get('cryptocurrencies', 'BTC,ETH,ETC,LTC,XRP,BCH')
 
-const notification = {
-  title: 'BTC Alert',
-  body: 'BTC just beat your target price!',
-  icon: path.join(__dirname, '../assets/images/bitcoin.png')
-}
 
 function buildHTMLFields(crypto, currency) {
   let row = `<div class="row">
@@ -33,27 +25,27 @@ function buildHTMLFields(crypto, currency) {
       </p>
     </div>
     <div id="right-container">
-      <button id="notifyBtn">Notify me when...</button>
+      <button id="${crypto.toLowerCase()}NotifyBtn">Notify me when...</button>
     </div>
   </div>`
   const div = document.getElementById('cryptocurrencies')
   div.innerHTML += row
 }
 
-function compareValue(previous, actual, currency){
-  const normalizedPrevious = Number(previous.replace(/<.*><.*>\s£|\,|<\/span>/g, ''))
-  let icon
+function getCurrencyIcon(currency) {
   switch(currency) {
     case "GBP":
-      icon = "£"
-      break;
+      return "£"
     case "USD":
-      icon = "$"
-      break;
+      return "$"
     case "EUR":
-      icon = "€"
-      break
+      return "€"
   }
+}
+
+function compareValue(previous, actual, currency){
+  const normalizedPrevious = Number(previous.replace(/<.*><.*>\s£|\,|<\/span>/g, ''))
+  icon = getCurrencyIcon(currency)
 
   if (normalizedPrevious < actual) {
     return '<span id="currentValue"><img src="../assets/images/rise.svg"> ' + icon + actual.toLocaleString('en') + '</span>'
@@ -68,14 +60,6 @@ function updateSubtext(crypto, currency) {
   return 'CURRENT ' + crypto + ' IN ' + currency
 }
 
-function toggleNotification(crypto, currentPrice) {
-  if (`${crypto}TargetPrice.innerHTML` && `${crypto}TargetPriceVal` < currentPrice) {
-    const myNotification = new window.Notification(notification.title, notification)
-  }
-  //   if (targetPrice.innerHTML && targetPriceVal < res.data.USD) {
-  //   const myNotification = new window.Notification(notification.title, notification)
-  // }
-}
 
 function getValues() {
   const currency = store.get('currency', 'GBP');
@@ -90,26 +74,32 @@ function getValues() {
         for (const crypto in res.data) {
           switch(crypto) {
             case "BTC":
+              setNotification(crypto, currency, Object.values(res.data.BTC)[0]);
               btcIntro.innerHTML = updateSubtext(crypto, currency)
               btcPrice.innerHTML = compareValue(btcPrice.innerHTML, Object.values(res.data.BTC)[0], currency)
               break;
             case "LTC":
+              setNotification(crypto, currency, Object.values(res.data.LTC)[0]);
               ltcIntro.innerHTML = updateSubtext(crypto, currency)
               ltcPrice.innerHTML = compareValue(ltcPrice.innerHTML, Object.values(res.data.LTC)[0], currency)
               break;
             case "ETH":
+              setNotification(crypto, currency, Object.values(res.data.ETH)[0]);
               ethIntro.innerHTML = updateSubtext(crypto, currency)
               ethPrice.innerHTML = compareValue(ethPrice.innerHTML, Object.values(res.data.ETH)[0], currency)
               break;
             case "BCH":
+              setNotification(crypto, currency, Object.values(res.data.BCH)[0]);
               bchIntro.innerHTML = updateSubtext(crypto, currency)
               bchPrice.innerHTML = compareValue(bchPrice.innerHTML, Object.values(res.data.BCH)[0], currency)
               break;
             case "XRP":
+              setNotification(crypto, currency, Object.values(res.data.XRP)[0]);
               xrpIntro.innerHTML = updateSubtext(crypto, currency)
               xrpPrice.innerHTML = compareValue(xrpPrice.innerHTML, Object.values(res.data.XRP)[0], currency)
               break;
             case "ETC":
+              setNotification(crypto, currency, Object.values(res.data.ETC)[0]);
               etcIntro.innerHTML = updateSubtext(crypto, currency)
               etcPrice.innerHTML = compareValue(etcPrice.innerHTML, Object.values(res.data.ETC)[0], currency)
               break;
@@ -130,22 +120,53 @@ for (c of cryptocurrencies.slice(0, -1).split(',')) {
 getValues()
 setInterval("getValues()", 30000);
 
-notifyBtn.addEventListener('click', function(event) {
-  const modalPath = path.join('file://', __dirname, 'add.html')
-  let win = new BrowserWindow(
-    {
-      frame: false, 
-      transparent: true, 
-      alwaysOnTop: true, 
-      width: 400,
-      height: 200
-    })
-  win.on('close', function(){ win = null })
-  win.loadURL(modalPath)
-  win.show()
-})
 
-ipc.on('targetPriceVal', function(event, arg){
-  targetPriceVal = Number(arg)
-  targetPrice.innerHTML = '$' + targetPriceVal.toLocaleString('en')
+function setNotification(crypto, currency, currentPrice) { 
+  const notifyBtn = document.getElementById(`${crypto.toLowerCase()}NotifyBtn`)
+  const targetPrice = document.getElementById(`${crypto.toLowerCase()}TargetPrice`)
+  let targetPriceVal 
+  icon = getCurrencyIcon(currency)
+
+  // Calls add.html so user can input the desired target price
+  console.log(document.getElementById(`${crypto.toLowerCase()}NotifyBtn`))
+  notifyBtn.addEventListener('click', function(event) {
+    const modalPath = path.join('file://', __dirname, 'add.html')
+    let win = new BrowserWindow(
+      {
+        frame: false, 
+        transparent: true, 
+        alwaysOnTop: true, 
+        width: 400,
+        height: 200
+      })
+    win.on('close', function(){ win = null })
+    win.loadURL(modalPath)
+    win.show()
+  })
+
+
+
+  // Get the desired targed price that was input on add.html
+  ipc.on('targetPriceVal', function(event, arg){
+    targetPriceVal = Number(arg)
+    targetPrice.innerHTML = icon + targetPriceVal.toLocaleString('en')
+  })
+
+
+  // create notification if target price was beat. 
+  // Here we turn targetPrice into a Number and 
+  // replace anything thats not a digit to and empty string.
+  if (Number(targetPrice.innerHTML.replace(/\D+/g, '')) && ! 0  < currentPrice) {
+    const notification = {
+      title: `${crypto} Alert`,
+      body: `${crypto} just reached your target price!`,
+      icon: path.join(__dirname, `../assets/images/${crypto.toLowerCase()}.svg`)
+    }
+    const myNotification = new window.Notification(notification.title, notification)
+  }
+}
+
+
+ipc.on('update-notify-value', function(event, arg){
+  win.webContents.send(`targetPriceVal`, arg)
 })
